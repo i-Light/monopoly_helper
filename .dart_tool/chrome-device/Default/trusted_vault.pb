9dfd3ca89df5867b40585c21b85f4f3762d2ee6b
i-Light <111108515+i-Light@users.noreply.github.com>
2026-08-19 17:12:17 +0300

    created bransh

---
+++ .dart_tool/chrome-device/Default/trusted_vault.pb
@@ -0,0 +1,4 @@
+
+
+
+8a41398bfa09fa35 e5a1cc7d512bfbf2a86ad4354594c356,Kyzc9GdWCgtCaZuiEw9mvyuWUYljyRHcZteIdKlPILk=
---
+++ .dart_tool/flutter_build/cfb5da628cf3a4b37322665c06a860f8/web_resources.d
@@ -0,0 +1,1 @@
+ C:\\programming\\flutter\\monopoly_helper\\build\\web\\manifest.json:  C:\\programming\\flutter\\monopoly_helper\\web\\index.html C:\\programming\\flutter\\monopoly_helper\\web\\manifest.json
